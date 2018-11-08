c65ceb9a237825621240aafb72aea65650ec9d04
Kevin <kevinahpine@gmail.com>
2018-11-08 20:50:08 +0100

    Ajoute vue listPostsView.php

---
+++ view/listPostsView.php
@@ -0,0 +1,55 @@
+<?php $title = 'Billet simple pour l\'Alaska'; ?>
+
+
+<?php ob_start(); ?>
+<div class="row back">
+    <div class="col-xs-12">
+        <div class="row">
+            <div class="text-center jumbotron">
+                <div class="container">
+                    <h1>Billet Simple pour l'Alaska</h1>
+                </div>
+            </div>
+        </div>
+
+        <?php
+
+foreach ($row as $key => $value) {
+    
+
+
+
+   
+?>
+            <div class="row col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3  chapter">
+                <div class="text-center">
+                   
+                    <h3>
+                        <?= nl2br(htmlspecialchars($value->_title)) ?>
+                    </h3>
+
+                    <p>
+                        <?= nl2br(htmlspecialchars($value->_content)) ?>
+
+                    </p>
+
+                </div>
+            </div>
+
+
+
+            <?php
+
+}
+
+
+?>
+      
+    </div>
+   
+  
+</div>
+<?php $content = ob_get_clean(); ?>
+
+
+<?php require('template.php'); ?>
