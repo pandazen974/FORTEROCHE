22b037106f9c33bd7e085ff7afbd27e15e444737
Kevin <kevinahpine@gmail.com>
2018-11-08 20:29:19 +0100

    Modifie fichier PostController.php

--- model/controllers/PostController.php
+++ model/controllers/PostController.php
@@ -18,45 +18,6 @@ class PostController{
 
 
 }
-OBOBOBOBOBOBOAOAOAOAOAOBOBOBOBOB[3~[3~[3~i[200~<?php
-namespace Forteroche\controllers;
-use\Forteroche\config\Database;
-use \Forteroche\blog\PostManager;
 
 
-
-class PostController{
-        public function display(){
-    $database = new Database();
-    $db = $database->getConnection();
-    $postManager= new PostManager($db);
-    $row = $postManager->readAll();
-    require_once('view/listPostsView.php');
-
-
-        }
-
-
-}
-^[OB^[OB^[OB^[OB^[OB^[OB^[OA^[OA^[OA^[OA^[OA^[OB^[OB^[OB^[OB^[OB^?^?^?^?^?^?^?^?
-^?^?^?^?^?^?^?^?^?^?^?^?^?^?^?^?^[[3~^[[3~^[[3~i^?^?
-namespace Forteroche\controllers;
-use\Forteroche\config\Database;
-use \Forteroche\blog\PostManager;
-
-
-
-class PostController{
-        public function display(){
-    $database = new Database();
-    $db = $database->getConnection();
-    $postManager= new PostManager($db);
-    $row = $postManager->readAll();
-    require_once('view/listPostsView.php');
-
-
-        }
-
-
-}
 
