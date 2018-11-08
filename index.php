dc1293faf38fddec7f7e403a5f82c3baf4cc35c4
Kevin <kevinahpine@gmail.com>
2018-11-08 20:46:16 +0100

    Ajoute fichier index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+use \Forteroche\Autoloader;
+use\Forteroche\controllers\PostController;
+
+require'model/Autoloader.php';
+
+Autoloader::register();
+
+
+
+try { // On essaie de faire des choses
+    if (isset($_GET['action'])) {
+
+        if ($_GET['action'] == 'display') {
+        
+            $postController->display();
+
+        }
+        
+        elseif ($_GET['action']=='goToLogIn'){
+            goToLogIn();
+        }
+        
+        
+
+    }
+
+    else {
+        $postController=new PostController();
+       $postController->display();
+
+    }
+
+}
+
+catch(Exception $e) { // S'il y a eu une erreur, alors...
+
+    echo 'Erreur : ' . $e->getMessage();
+
+}
