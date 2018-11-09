f7a3886a85562594b46fa70e4d0a55eeeaecf871
onzol <onzol@LAPTOP-GDLSB58Q>
2018-11-09 10:43:23 +0100

    test

---
+++ Projet_4/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use \Forteroche\Autoloader;
+use\Forteroche\controllers\PostController;
+
+require'model/Autoloader.php';
+
+Autoloader::register();
+
+
+
+try { // On essaie de faire des choses
+    if (isset($_GET['action'])) {
+
+        if ($_GET['action'] == 'display') {
+        
+            $postController->display();
+
+        }
+        
+        elseif ($_GET['action']=='readPost'){
+            $postController=new PostController();
+            $postController->readPost();
+        }
+        
+        
+
+    }
+
+    else {
+       $postController=new PostController();
+       $postController->display();
+
+    }
+
+}
+
+catch(Exception $e) { // S'il y a eu une erreur, alors...
+
+    echo 'Erreur : ' . $e->getMessage();
+
+}
