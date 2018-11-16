12a650d6b40906667d0740635a8ce9ef747aae89
onzol <onzol@LAPTOP-GDLSB58Q>
2018-11-16 11:35:50 +0100

    Ajoute fonction readOnePost l.19 ok

--- controllers/post/PostController.php
+++ controllers/post/PostController.php
@@ -6,16 +6,23 @@ use \Forteroche\model\blog\PostManager;
 
 
 class PostController{
-	public function display(){
+    
+public function display(){
     $database = new Database();
     $db = $database->getConnection();
     $postManager= new PostManager($db);
     $row = $postManager->readAll();
     require_once('view/listPostsView.php');
-   
+    
+}
 
-	}
-
+public function readOnePost(){
+    $database = new Database();
+    $db = $database->getConnection();
+    $postManager= new PostManager($db);
+    $post=$postManager->readSelectedPost($_GET['id']);
+    require_once('view/postView.php');
+}
 
 }
 
