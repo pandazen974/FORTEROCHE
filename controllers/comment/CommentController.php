0c3f913f558dfc513d9161caf35516c82a266232
onzol <onzol@LAPTOP-GDLSB58Q>
2018-11-27 15:39:13 +0100

    Ajoute fonction addComment l.22

--- controllers/comment/CommentController.php
+++ controllers/comment/CommentController.php
@@ -7,15 +7,32 @@ use\Forteroche\model\blog\PostManager;
 
 
 class CommentController{
-	public function displayComments(){
+    public function displayComments(){
     $database = new Database();
     $db = $database->getConnection();
+    
     $postManager= new PostManager($db);
     $post=$postManager->readSelectedPost($_GET['id']);
+    
     $commentManager= new CommentManager($db);
     $comments = $commentManager->readCommentsFromPost($_GET['id']);
     require_once('view/postView.php');
 	}
+        
+    public function addComment(){
+    $database = new Database();
+    $db = $database->getConnection();
+    
+    $postManager= new PostManager($db);
+    $post=$postManager->readSelectedPost($_GET['id']);
+   
+    $commentManager=new CommentManager($db);
+    $comment=new Comment(['postId'=>$_GET['id'],'author'=>$_POST['author'],'comment'=>$_POST['comment']]);
+    $newComment=$commentManager->createOneComment($comment);
+    $comments = $commentManager->readCommentsFromPost($_GET['id']);
+    require_once('view/postView.php');
+    
+}
 }
 
 
