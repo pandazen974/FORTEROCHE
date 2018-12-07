bd00be74123661ce6bc330c889ab22a04746557d
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-07 14:24:38 +0100

    Ajoute fonctions createUser l.17 et verifyUserExistence l.34

--- model/admin/UserManager.php
+++ model/admin/UserManager.php
@@ -14,4 +14,40 @@ Class UserManager{
         $this->conn = $db;
     }
     
+    public function createUser(User $user){
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    pseudo=:pseudo, pass=:pass, email=:email";
+ 
+        $stmt = $this->conn->prepare($query);
+        $pseudo= htmlspecialchars($user->pseudo());
+        $pass= htmlspecialchars($user->pass());
+        $email= htmlspecialchars($user->email());
+        $protected=password_hash($pass,PASSWORD_DEFAULT);
+        $stmt->bindParam(':pseudo',$pseudo);
+        $stmt->bindParam(':pass',$protected);
+        $stmt->bindParam(':email',$email);
+        $stmt->execute();
+        
+    }
+    public function verifyUserExistence($pseudo){
+        $query = "SELECT*
+            FROM
+                " . $this->table_name . "
+            WHERE
+                pseudo = :pseudo";
+   
+    $stmt = $this->conn->prepare( $query );
+    $stmt->bindParam(':pseudo', $pseudo);
+    $stmt->execute();
+    $theUser = $stmt->fetch(\PDO::FETCH_ASSOC);
+    if ($theUser===false){
+        $user=null;
+    }else{
+    $user=new User($theUser);
+    }
+    return $user;
+    }
+    
 }
