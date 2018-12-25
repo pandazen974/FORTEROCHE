1b8a9c2e03a2074b44c9eec8811e856631b8ad8c
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-25 19:39:16 +0100

    Modif display/Ajout fonctions goToEditView l.28 et editPost l.38

--- controllers/post/PostController.php
+++ controllers/post/PostController.php
@@ -2,18 +2,19 @@
 namespace Forteroche\controllers\post;
 use\Forteroche\model\config\Database;
 use \Forteroche\model\blog\PostManager;
-
-
+use \Forteroche\model\blog\Post;
 
 class PostController{
-    
-public function display(){
+   
+public function displayPosts(){
     $database = new Database();
     $db = $database->getConnection();
     $postManager= new PostManager($db);
     $row = $postManager->readAll();
+        if (empty($row)){
+          $erreur="Aucun article n'a été publié!";
+        }
     require_once('view/listPostsView.php');
-    
 }
 
 public function readOnePost(){
@@ -23,6 +24,32 @@ public function readOnePost(){
     $post=$postManager->readSelectedPost($_GET['id']);
     require_once('view/postView.php');
 }
+
+public function goToEditView(){    
+    if (isset($_GET['id'])){
+        $database = new Database();
+        $db = $database->getConnection();
+        $postManager= new PostManager($db);    
+        $post=$postManager->readSelectedPost($_GET['id']);
+        return $post;
+    }
+}
+
+public function editPost(){
+    $database = new Database();
+    $db = $database->getConnection();
+    $postManager= new PostManager($db);
+    if (isset($_POST['id'])){
+        $post=new Post(['id'=>$_POST['id'],'title'=>$_POST['title'],'content'=>$_POST['content']]);
+        $newPost=$postManager->updatePost($post);
+    }
+    else{
+        $post=new Post(['title'=>$_POST['title'],'content'=>$_POST['content']]);
+        $newPost=$postManager->createPost($post);
+    }
+    $row = $postManager->readAll();
+    require_once('view/listPostsView.php');
+        }
 
 }
 
