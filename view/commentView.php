2cc4e4af20c9c5b9dd6d77fc0dc37b58f584de09
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-26 13:58:35 +0100

    Mise à jour vue commentaires

--- view/commentView.php
+++ view/commentView.php
@@ -2,16 +2,20 @@
 if(isset($comments)){
         foreach ($comments as $key => $value) {
         ?>
-
+                     
                     <p>
                         <i>
                         Le <?= nl2br(htmlspecialchars($value->commentDate())) ?>
                        </i><br/>
-                       
                         <strong><?= nl2br(htmlspecialchars($value->author())) ?></strong>:
+                        <?php $report=$value->report();
+                        if($report==='0'){?>
                         <?= nl2br(htmlspecialchars($value->comment())) ?><br/>
-                            <a href="index.php?action=reportComment&amp;id=<?=$value->id() ?>"> <span class="glyphicon glyphicon-flag"></span>Signaler</a>
-
+                            <a href="index.php?action=flagComment&amp;id=<?=$value->id() ?>&amp;postId=<?=$value->postId() ?>"> <span class="glyphicon glyphicon-flag"></span>Signaler</a>
+                        <?php 
+                        }else{?>
+                            <span id="report">"Ce commentaire a été signalé et est en attente de modération."</span>
+                        <?php } ?>
                     </p>
 
                     <?php
