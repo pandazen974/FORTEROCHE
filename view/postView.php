acca96f980822d96ed312d8f50d65f9485cfc0a1
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-26 13:55:29 +0100

    Mise à jour vue publication

--- view/postView.php
+++ view/postView.php
@@ -5,70 +5,42 @@
 
 <div class='row back '>
     <div class="col-xs-12 box">
-
-        <div class="row">
-            <div class="col-xs-12">
-                <a href="index.php">Retour aux épisodes</a>
-            </div>
-            
-        </div>
-
-        <br/>
         <div class="row box">
             <div class="col-lg-12">
                 <div class="chapter">
-                <h3>
-
-                    <?= nl2br(htmlspecialchars($post->title())) ?>
-                </h3>
-
-                <span class="pull-right"><i><?= nl2br(htmlspecialchars($post->postDate())) ?></i></span><br/>
-
-                <div class="allbackground">
-
-                    <?= nl2br(htmlspecialchars($post->content())) ?><br/>
-                    <?php if (!isset($comments)){ ?>
-                    <a href="index.php?action=displayComments&amp;id=<?= $post->id() ?>" class="pull-right" id="comment-link">Commentaires</a><br/>
-                    <?php
-    }
-                    ?>
-                        
-                </div>
+                    <h3>
+                        <?= nl2br(htmlspecialchars($post->title())) ?>
+                    </h3>
+                    <span class="pull-right"><i><?= nl2br(htmlspecialchars($post->postDate())) ?></i></span><br/>
+                        <div class="allbackground">
+                            <?= nl2br(htmlspecialchars($post->content())) ?><br/>
+                            <?php  if (!isset($comments)){ ?>
+                                <a href="index.php?action=displayComments&amp;id=<?= $post->id() ?>" class="pull-right" id="comment-link">Commentaires</a><br/>
+                            <?php }?>
+                        </div>
                     <br/>
-                    <div class="row box">
-            <div class="col-xs-12">
-              <?php 
-                include ('commentView.php');
-              ?>
+                        <div class="row box">
+                            <div class="col-xs-12">
+                                <?php 
+                                    include ('commentView.php');
+                                ?>
+                            </div>
+                        </div>
+                 </div>
+
+                <div  class="col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3 formbox text-center">
+                    <h2>Ajouter un commentaire</h2>
+                    <form action="index.php?action=addComment&amp;id=<?= $post->id() ?>" method="post">
+                        <label for="author">Pseudo:</label><br />
+                        <input type="text" id="author" name="author" /><br/>
+                        
+                        <label for="comment">Commentaire:</label><br />
+                        <textarea id="comment" name="comment" ></textarea><br/>
+
+                        <button class="btn btn-primary" type="submit">Ajouter <span class="glyphicon glyphicon-comment" ></span></button>
+                    </form>
+                </div>
             </div>
-            </div>
-                 
-
-            </div>
-
-
-
-        
-            
-            <div  class="col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3 formbox text-center">
-                <h2>Ajouter un commentaire</h2>
-                <form action="index.php?action=addComment&amp;id=<?= $post->id() ?>" method="post">
-
-
-
-                    <label for="author">Pseudo:</label><br />
-                    <input type="text" id="author" name="author" /><br/>
-
-
-                    <label for="comment">Commentaire:</label><br />
-                    <textarea id="comment" name="comment" ></textarea><br/>
-
-                    <button class="btn btn-primary" type="submit">Ajouter <span class="glyphicon glyphicon-comment" ></span></button>
-                </form>
-            </div>
-        </div>
-            
-
         </div>
     </div>
 </div>
