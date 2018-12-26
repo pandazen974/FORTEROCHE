90ec2702f8fd42aa7d136c31dda49d2df067b754
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-26 13:49:16 +0100

    Mise à jour vue liste des publications

--- view/listPostsView.php
+++ view/listPostsView.php
@@ -1,6 +1,5 @@
 <?php $title = 'Billet simple pour l\'Alaska'; ?>
 
-
 <?php ob_start(); ?>
 
 <div class="row back">
@@ -13,52 +12,39 @@
             </div>
         </div>
 
-        <?php
-        
-
-foreach ($row as $key => $value) {
-    
-   
-
-
-   
+<?php      
+    if (!empty($row)){
+        foreach ($row as $key => $value) {   
 ?>
-            <div class="row col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3  chapter">
-                <div class="text-center">
-                   
-                    <h3>
-                        <?= nl2br(htmlspecialchars($value->title())) ?>
-                    </h3>
-                    <span class="pull-right"><i><?= nl2br(htmlspecialchars($value->postDate())) ?></i></span>
-                    <br/>
-
-                    <div class="allbackground">
-                        <?= $value->content() ?>
-                         <div class="text-right" id="read">
-                    <a href="index.php?action=readOnePost&amp;id=<?= $value->id() ?>" >Lire</a>
+    <div class="row col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3  chapter">
+        <div class="text-center">
+            <h3>
+                <?= nl2br(htmlspecialchars($value->title())) ?>
+            </h3>
+            <span class="pull-right"><i><?= nl2br(htmlspecialchars($value->postDate())) ?></i></span><br/>
+            <div class="allbackground">
+                <?= substr($value->content(), 0,500 ).'...'; ?>
+                <div class="text-right" id="read">
+                    <a href="index.php?action=readOnePost&amp;id=<?= $value->id() ?>" >Lire la suite</a>
                 </div>
-
-                    </div>
-                    
-                    <br/>
-
-                </div>
-               
-            </div>
-
-
-
-            <?php
-
-}
-
-
-?>
-      
+            </div><br/>
+        </div>
     </div>
-   
-  
+
+<?php
+        }
+    }else{ ?>
+        <div class="row col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3  chapter">
+                <div class="text-center">
+                    <span><?php echo $erreur ?></span>
+                </div>
+        </div>
+<?php
+        }
+?>
+    </div>
 </div>
+
 <?php $content = ob_get_clean(); ?>
 
 
