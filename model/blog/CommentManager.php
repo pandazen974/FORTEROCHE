825f61367de442596a129c478b7ffae4f3b334fc
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-26 11:57:08 +0100

    Ajout fonctions reportComment l.102/readReportedComments l.72

--- model/blog/CommentManager.php
+++ model/blog/CommentManager.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Forteroche\model\blog;
+use Forteroche\model\blog\Comment;
 
 class CommentManager{
  
@@ -10,32 +11,29 @@ class CommentManager{
  
    
  
-    public function __construct($db){
+public function __construct($db){
         $this->conn = $db;
-    }
+}
  
 function createOneComment(Comment $comment){ 
-    //write query
-        $query = "INSERT INTO
+    $query = "INSERT INTO
                     " . $this->table_name . "
-                SET
-                    postId=:postId, author=:author, comment=:comment, commentDate= NOW(), report=0";
+            SET
+                postId=:postId, author=:author, comment=:comment, commentDate= NOW(), report=0";
  
-        $stmt = $this->conn->prepare($query);
-        $postId=$comment->postId();
-        $author=$comment->author();
-        $content=$comment->comment();
+    $stmt = $this->conn->prepare($query);
+    $postId=$comment->postId();
+    $author=$comment->author();
+    $content=$comment->comment();
         
-        $stmt->bindParam(':postId', $postId);
-        $stmt->bindParam(':author', $author);
-        $stmt->bindParam(':comment',$content);
-        $stmt->execute();
-        
-        
-    }
+    $stmt->bindParam(':postId', $postId);
+    $stmt->bindParam(':author', $author);
+    $stmt->bindParam(':comment',$content);
+    $stmt->execute();   
+}
     
- public function readCommentsFromPost($postId){
-        $query = "SELECT*, DATE_FORMAT(commentDate, ' %d/%m/%Y à %H:%i:%s') as commentDate
+public function readCommentsFromPost($postId){
+    $query = "SELECT*, DATE_FORMAT(commentDate, ' %d/%m/%Y à %H:%i:%s') as commentDate
             FROM
                 " . $this->table_name . "
             WHERE
@@ -47,13 +45,73 @@ function createOneComment(Comment $comment){
     while($commentRow = $stmt->fetch(\PDO::FETCH_ASSOC)){
     $comments[]=new Comment($commentRow);
     }
-    return $comments;
+    if (empty($comments)){
+        $comments=null;
     }
-    
-function report(){
-    
+    return $comments;
 }
 
- 
+public function readSelectedComment($id){
+    $query = "SELECT *
+        
+            FROM
+                " . $this->table_name . "
+            WHERE
+                id = :id
+            LIMIT
+                0,1";
+   
+    $stmt = $this->conn->prepare( $query );
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $line = $stmt->fetch(\PDO::FETCH_ASSOC);
+    $comment=new Comment($line);
+    return $comment;
+}
+
+public function readReportedComments(){
+    $query = "SELECT *, DATE_FORMAT(commentDate, ' %d/%m/%Y à %H:%i:%s') as commentDate
+
+            FROM
+                " . $this->table_name . "
+                    
+            WHERE
+            report=1;
+            
+            ORDER BY
+                id";
+
+    $stmt = $this->conn->prepare( $query );
+    $stmt->execute();
+   
+    while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+    {
+        
+        $line[]=new Comment($donnees);
+
+
+
+    }
+   if (empty($line)){
+       $line=null;
+   }
+   
+   return $line;
+}
+
+public function reportComment(Comment $comment){
+    $query = "UPDATE comments
+            SET report=:report
+            WHERE id = :id";
+    $stmt = $this->conn->prepare($query);
+    $id=$comment->id();
+    $stmt->bindParam(':id',$id);
+    $stmt->bindValue(':report',1);
+    $stmt->execute();
+        
+}  
+
+
+
 }
 
