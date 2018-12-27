c6099b9091309df9f243b0540e0e3ef20163705b
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-27 18:46:05 +0100

    Modifie pour le responsive

--- view/adminListView.php
+++ view/adminListView.php
@@ -2,7 +2,7 @@
     <?php
         foreach ($row as $key => $value) {
     ?>
-        <div class="row col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3  chapter">
+        <div class="row col-md-6 col-md-6 col-md-offset-3  chapter">
             <div class="text-center">
                 <h3>
                     <?= nl2br(htmlspecialchars($value->title())) ?>
