7969dfebadce4f9d8362f1e2bbe55cb253b42836
onzol <onzol@LAPTOP-GDLSB58Q>
2018-12-27 17:29:08 +0100

    Modif vue pour responsive

--- view/adminEditView.php
+++ view/adminEditView.php
@@ -1,14 +1,18 @@
 <div id="edit-content" class="col-md-10 text-center">
        <form action="index.php?action=displayDashboard&amp;admin=editPost" method="post">
-           
+           <div class="row">  
+               <div class="col-xs-8 col-sm-4 col-md-2 col-md-offset-10 ">
         <?php if (isset($post)){?> <input type='hidden' name='id' value=<?= $post->id()?>> 
-                        <button id="edit-button" class="btn btn-primary pull-right" type="submit"><span class="glyphicon glyphicon-edit" ></span> Editer</span></button>
-        <?php }else{?>  <button id="edit-button" class="btn btn-primary pull-right" type="submit"><span class="glyphicon glyphicon-pencil" ></span> Ajouter</span></button><?php } ?>  
-               <br/>
+                        <button id="edit-button" class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-edit" ></span> Editer</span></button>
+        <?php }else{?>  <button id="edit-button" class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-pencil" ></span> Ajouter</span></button><?php } ?> 
+           
+               </div>
+           </div>
+        <br/>
        <label for="title" class="edit-menu">Votre titre:</label>
-       <input type="text" id="title" name="title" style="width:400px"  value="<?php if(isset($post)){echo $post->title();} ?>" /><br/>  
+       <input type="text" id="title"  name="title" value="<?php if(isset($post)){echo $post->title();} ?>" /><br/>  
        
-       <label for="content" class="edit-menu">Votre épisode:</label><br/>
+       <label for="content" class="edit-menu">Votre épisode:</label>
        <textarea name="content" id="edit" > <?php if(isset($post)){echo $post->content();} ?></textarea><br/>
        
        </form>
