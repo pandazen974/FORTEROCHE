f6e2bd6f2e6de870b984424c60ea27fc3713a515
onzol <onzol@LAPTOP-GDLSB58Q>
2019-01-02 11:26:30 +0100

    Mise à a jour vue pour supprimer les épisodes

--- view/adminDeleteView.php
+++ view/adminDeleteView.php
@@ -1,23 +1,20 @@
 <div id="contentbox" class="col-md-10">
+    <div class="row col-md-6 col-md-6 col-md-offset-3  chapter">
+        <div class="text-center edit-view">
 <?php
 foreach ($row as $key => $value) {
 ?>
-    <div class="row col-md-6 col-md-6 col-md-offset-3  chapter">
-        <div class="text-center">
-            <h3>
-                <?= nl2br(htmlspecialchars($value->title())) ?>
-            </h3>
-            <div class="allbackground">
-                <?= substr($value->content(), 0,500 ).'...'; ?>
-            </div>
+    
+            <p>
+                <?= nl2br(htmlspecialchars($value->title())) ?><br/>
+                <a href="index.php?action=displayDashboard&amp;admin=erasePost&AMP;id=<?= $value->id() ?>" class="edit-link" ><span class="glyphicon glyphicon-trash"></span> Supprimer</a>
+            </p>
             <br/>
-            <div class="text-right" id="read">
-                <a href="index.php?action=displayDashboard&amp;admin=erasePost&AMP;id=<?= $value->id() ?>" ><span class="glyphicon glyphicon-trash"></span> Supprimer</a>
-            </div>
-            <br/>
-        </div>
-    </div>
+                
+       
 <?php
 }
 ?>
+        </div>
+  </div>
 </div>
