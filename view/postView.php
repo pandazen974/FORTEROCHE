c3618c992ba0a0492f3aa3ad5ef7d6f7948f7976
onzol <onzol@LAPTOP-GDLSB58Q>
2019-01-09 11:06:38 +0100

    Mise à jour vue

--- view/postView.php
+++ view/postView.php
@@ -15,9 +15,9 @@
                     <span class="pull-right"><i><?= $post->postDate() ?></i></span><br/>
                         <div class="allbackground">
                             <?= $post->content() ?><br/>
-                            <?php  if (!isset($comments)){ ?>
+                            <?php  if (!isset($info)){if(!isset($comments)){ ?>
                                 <a href="index.php?action=displayComments&amp;id=<?= $post->id() ?>" class="pull-right" id="comment-link">Commentaires</a><br/>
-                            <?php }?>
+                            <?php }}else{?><span class="pull-right"><?=$info?></span><?php } ?>
                         </div>
                     <br/>
                         <div class="row box">
@@ -31,7 +31,7 @@
             <?php } ?>
                 <div  class="col-xs-12 col-sm-12  col-md-6 col-md-6 col-md-offset-3 formbox text-center">
                     <h2>Ajouter un commentaire</h2>
-                    <form action="index.php?action=addComment&amp;id=<?= $post->id() ?>" method="post">
+                    <form action="index.php?action=addComment&amp;<?php if(isset($post)){?>id=<?= $post->id()?> <?php }?>" method="post">
                         <label for="author">Pseudo*:</label><br />
                         <input type="text" id="author" name="author" <?php if (isset($erreur)){?>value="<?= htmlspecialchars($_POST['author'])?>"<?php }?> /><br/>
                         
